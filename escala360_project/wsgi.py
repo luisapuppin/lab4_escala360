68f479f1613c329ec2ed125bde74f06a0eecfdc2
luisapuppin <puppinha@gmail.com>
2025-10-23 14:09:44 -0300

    Update wsgi.py
    Adaptação wsgi.py para build no vercel.

--- escala360_project/wsgi.py
+++ escala360_project/wsgi.py
@@ -14,3 +14,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'escala360_project.settings')
 
 application = get_wsgi_application()
+
+# Apenas para build no vercel
+app = application
