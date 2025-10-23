b867eef15e3cb8a440cbcdfecd4649e4eda4c159
luisapuppin <puppinha@gmail.com>
2025-10-23 14:04:53 -0300

    Update build.sh
    Update do comando python para python3

--- build.sh
+++ build.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 # Instalar as dependências
-echo "Instalando dependências..."
-pip install -r requirements.txt
+# echo "Instalando dependências..."
+# pip install -r requirements.txt
 
 # Executar as migrações do banco de dados
 echo "Executando migrações..."
-python manage.py makemigrations --noinput
-python manage.py migrate --noinput
-python manage.py criar_funcoes_locais
-python manage.py importar_dados_sql
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --noinput
+python3 manage.py criar_funcoes_locais
+python3 manage.py importar_dados_sql
 
 echo "Build concluído com sucesso."
